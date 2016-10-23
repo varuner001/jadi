11dd1a11645f02ca54f3ba5c4a4c2ff25d2c5d7d
varunR <ragunathanvarun@gmail.com>
2016-10-22 19:11:39 -0700

    Removing node modules

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('users_home', { title: 'Express - Users' });
 });
 
 module.exports = router;
